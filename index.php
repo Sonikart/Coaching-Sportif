5b48e9d4b61b46ac9496efe3b2ca337079d2c341
Sonik <35891816+Sonikart@users.noreply.github.com>
2018-04-11 18:08:38 +0200

    Push
    Publication

---
+++ index.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/stylesheets/style.css">
+    <link rel="stylesheet" href="assets/stylesheets/responsive.css">
+    <link rel="stylesheet" href="assets/stylesheets/animate.css">
+    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <title>Coach Sportif</title>
+</head>
+<body>
+    <div class="site-container">
+        <div class="site-pusher">
+            <header class="header">
+                <a href="#" class="header__icon" id="header__icon"></a>
+                <a href="#" class="header__logo">Coach<font class="gold">Sportif</font>.fr</a>
+                <nav class="menu">
+                    <a href="#">L'ACCUEIL</a>
+                    <a href="#">NOTRE PROGRAMME</a>
+                    <a href="#">NOTRE GALLERIE</a>
+                    <a href="#">LES NEWS</a>
+                    <a href="#">CONTACTEZ NOUS</a>
+                </nav>
+            </header>
+            <div class="site-content">
+                <section class="top__hero">
+                    <div class="top__hero__content">
+                        <h4 class="title__hero animated flash">TRANSFORMEZ <font class="gold">VOTRE</font> CORPS !</h4>
+                        <p class="sous__title__hero animated flash">Avec notre programme personnalisé en ligne..</p>
+                        <div class="form-group">
+                            <button class="btn btn-warning">
+                                <i class="fas fa-pencil-alt"></i> Prendre un Rendez-Vous
+                            </button>
+                            <button class="btn btn-white">
+                                <i class="fab fa-angellist"></i> Notre Programme
+                            </button>
+                        </div>
+                    </div>
+                </section>
+                <section class="about__content">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="about__picture"> </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="about__text">
+                                    <h4 class="title__about">Qui suis-je? <em>A propos de moi</em></h4>
+                                    <hr>
+                                    <p class="text__p">Diplômé d’Etat des métiers de la forme et fort d’une expérience personnelle de plus de 10 ans dans la musculation, je vous apporte mon soutien et mes connaissances pour que chaque séance vous rapproche un peu plus de votre objectif. Sportif depuis ma plus tendre jeunesse, je me suis spécialisé dans le coaching en club de remise en forme et à distance. Plus qu’un métier, c’est devenu pour moi une passion. C’est ce goût du sport que je cherche à vous transmettre pour vous guider et vous faire progresser.</p>
+                                    <p class="text__p_b">Votre objectif deviendra le mien !</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row boxed__badge">
+                            <div class="col-md-3 content__badge one">
+                                <div class="box__badge">
+                                    <i class="fas fa-trophy"></i>
+                                    <p class="desc_badge">PROGRAMMES 100% personnalises</p>
+                                </div>
+                            </div>
+                            <div class="col-md-3 content__badge two">
+                                <div class="box__badge">
+                                    <i class="fab fa-facebook-messenger"></i>
+                                    <p class="desc_badge">Suivez la communauté sur Messenger</p>
+                                </div>
+                            </div>
+                            <div class="col-md-3 content__badge three">
+                                <div class="box__badge">
+                                    <i class="fas fa-list-ul"></i>
+                                    <p class="desc_badge">PRISE DE RENDEZ VOUS RAPIDE</p>
+                                </div>
+                            </div>
+                            <div class="col-md-3 content__badge four">
+                                <div class="box__badge">
+                                    <i class="fas fa-assistive-listening-systems"></i>
+                                    <p class="desc_badge">Un Coach a votre Ecoute</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <div id="site-cache" class="site-cache"></div>
+        </div>
+    </div>
+<script type="text/javascript" src="assets/js/jquery-3.3.1.js"></script>
+<script type="text/javascript" src="assets/js/app.js"></script>
+</body>
+</html>
